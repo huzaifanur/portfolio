9a8ed904fde6b164c57fb3a49dc551216b3a335d
huzaifanur <huzaifa98nur@gmail.com>
2023-05-02 14:39:15 +0500

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
+import { info } from "./index.module.css";
 
 export default function index() {
-  return <div>index</div>;
+  return <div className={info}>My Portfolio</div>;
 }
